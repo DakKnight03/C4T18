9dbecbd9a614eb2644cb2439db44b5d5a0741c12
DakKnight03 <votindu@gmail.com>
2019-05-12 16:10:39 +0700

    first commit

---
+++ sessions/session1/session1.py
@@ -0,0 +1,4 @@
+number1 = int(input("enter the first number: "))
+number2 = int(input("enter the second number: "))
+total = number1 + number2
+print("the sum of the first number and the second number is", total)
